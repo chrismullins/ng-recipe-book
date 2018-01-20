9145de28d05e5bc0a819a4a40dd40948a7b6b8ae
Christopher Mullins <christopherrmullins@gmail.com>
2018-01-20 17:00:04 -0500

    Add click listener to recipe on selected

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,9 +7,15 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeClicked = new EventEmitter<Recipe>();
+
+  onRecipeClicked($event) {
+    this.recipeClicked.emit($event);
+  }
+
   recipes: Recipe[] = [
     new Recipe('Hard Boiled Egg', 'the perfect hard-boiled egg recipe', 'https://i.imgur.com/738TCU9.jpg'),
-    new Recipe('Hard Boiled Egg', 'the perfect hard-boiled egg recipe', 'https://i.imgur.com/738TCU9.jpg')
+    new Recipe('Hard Boiled Egg2', 'another perfect hard-boiled egg recipe', 'https://i.imgur.com/738TCU9.jpg')
   ];
 
   constructor() { }
