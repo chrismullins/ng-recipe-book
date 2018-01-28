f55089cceed06fac3cc3273a6333f8de8ed1162d
Christopher Mullins <christopherrmullins@gmail.com>
2018-01-27 19:43:46 -0500

    Use services to update the shopping list

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,12 +1,28 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipes/recipe.model';
+import {Ingredient} from './shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Hard Boiled Egg', 'the perfect hard-boiled egg recipe', 'https://i.imgur.com/738TCU9.jpg'),
-    new Recipe('Hard Boiled Egg2', 'another perfect hard-boiled egg recipe', 'https://i.imgur.com/738TCU9.jpg')
+    new Recipe('Hard Boiled Egg',
+      'the perfect hard-boiled egg recipe',
+      'https://i.imgur.com/738TCU9.jpg',
+      [
+        new Ingredient('Egg', 1),
+        new Ingredient( 'Water', 5)
+      ]
+    ),
+    new Recipe('Hard Boiled Egg2',
+      'another perfect hard-boiled egg recipe',
+      'https://i.imgur.com/738TCU9.jpg',
+      [
+        new Ingredient('Egg2', 2),
+        new Ingredient('Water2', 5),
+        new Ingredient('Salt', 2)
+        ]
+    )
   ];
 
   getRecipes(): Recipe[] {
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,23 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {ShoppingListService} from '../../shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
+  // providers: [ShoppingListService]
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  onAddToShoppingList() {
+    for (let ingredient of this.recipe.ingredients) {
+      this.shoppingListService.addIngredient(ingredient);
+    }
+  }
+
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
