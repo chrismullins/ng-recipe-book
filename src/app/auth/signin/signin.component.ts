d479c85c8d8e848027db001185e13911dda1df7d
Christopher Mullins <christopherrmullins@gmail.com>
2018-03-04 13:43:52 -0500

    Signin with ngrx/store

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthService} from '../auth.service';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../../store/app.reducers';
+import  * as AuthActions from '../store/auth.actions';
 
 @Component({
   selector: 'app-signin',
@@ -9,13 +11,15 @@ import {AuthService} from '../auth.service';
 })
 export class SigninComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
   }
 
   onSignin(form: NgForm) {
-    this.authService.signinUser(form.value.email, form.value.password);
+    const email = form.value.email;
+    const password = form.value.password;
+    this.store.dispatch(new AuthActions.TrySignin({username: email, password: password}));
   }
 
 }
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -6,6 +6,7 @@ import {fromPromise} from 'rxjs/observable/fromPromise';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthEffects {
@@ -33,5 +34,30 @@ export class AuthEffects {
       ];
     });
 
-  constructor(private actions$: Actions) {}
+  @Effect()
+  authSignin = this.actions$
+    .ofType(AuthActions.TRY_SIGNIN)
+    .map((action: AuthActions.TrySignin) => {
+      return action.payload;
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+      return fromPromise(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      this.router.navigate(['/']);
+      return [
+        {
+          type: AuthActions.SIGNIN
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    });
+
+  constructor(private actions$: Actions, private router: Router) {}
 }
