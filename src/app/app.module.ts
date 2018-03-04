a8bb4a64802720c9b5b69c101eebf73d82fa0fa7
Christopher Mullins <christopherrmullins@gmail.com>
2018-03-03 20:06:34 -0500

    Reorganize to use app reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {AuthModule} from './auth/auth.module';
 import {CoreModule} from './core/core.module';
 import {HttpClientModule} from '@angular/common/http';
 import {StoreModule} from '@ngrx/store';
-import {shoppingListReducer} from './shopping-list/store/shopping-list.reducers';
+import { reducers } from './store/app.reducers';
 
 
 @NgModule({
@@ -30,7 +30,7 @@ import {shoppingListReducer} from './shopping-list/store/shopping-list.reducers'
     ShoppingListModule,
     AuthModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot(reducers)
   ],
   bootstrap: [AppComponent]
 })
--- src/app/auth/store/auth.reducers.ts
+++ src/app/auth/store/auth.reducers.ts
@@ -1,4 +1,4 @@
-import {Ingredient} from '../../shared/ingredient.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
   token: string;
@@ -10,6 +10,21 @@ const initialState: State = {
   authenticated: false
 };
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(state = initialState, action: AuthActions) {
+  switch(action.type) {
+    case AuthActions.SIGNUP:
+    case AuthActions.SIGNIN:
+      return {
+        ...state,
+        authenticated: true
+      };
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        token: null,
+        authenticated: false
+      };
+    default:
+      return state;
+  }
 }
